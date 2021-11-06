7a76e8e759f89fa613f93d4bfd6ddd8ad73d06f8
hoyeon <hoyun293@naver.com>
2021-11-06 10:47:16 +0900

    [Chunk1] Treemap 개발 (with 더미데이터)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,106 @@
 import './App.css'
 import React, { useRef, useEffect, useState } from 'react'
-import { select } from 'd3'
+import { select, selectAll, hierarchy, treemap } from 'd3'
+import { dummyData } from './dummyData'
+const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null)
+  useEffect(() => {
+    const observeTarget = ref.current
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect)
+      })
+    })
+    resizeObserver.observe(observeTarget)
+    return () => {
+      resizeObserver.unobserve(observeTarget)
+    }
+  }, [ref])
+  return dimensions
+}
+let stockTiles
+let hierarchyData = hierarchy(dummyData)
+  .sum((d) => d.value)
+  .sort((a, b) => b.value - a.value)
 
 function App() {
   const [data, setData] = useState([25, 30, 45, 60, 20])
+  const [isDrawn, setIsDrawn] = useState(false)
   const svgRef = useRef()
+  const wrapperRef = useRef()
+
+  const dimensions = useResizeObserver(wrapperRef)
+  let drawTreeMap = () => {
+    let createTreeMap = treemap().size([dimensions.width, dimensions.height])
+
+    createTreeMap(hierarchyData)
+    let stockData = hierarchyData.leaves()
+    if (!isDrawn) {
+      stockTiles = select(svgRef.current)
+        .selectAll('g')
+        .data(stockData)
+        .join('g')
+        .attr('transform', (d) => {
+          return 'translate(' + d['x0'] + ', ' + d['y0'] + ')'
+        })
+
+      stockTiles
+        .append('rect')
+        .attr('class', 'leaf')
+        .attr('fill', (d) => d['data']['color'])
+        .attr('width', (d) => d['x1'] - d['x0'])
+        .attr('height', (d) => d['y1'] - d['y0'])
+
+      stockTiles
+        .append('text')
+        .text((d) => d['data']['name'])
+        .attr('y', 20)
+    } else {
+      let coorMap = new Map()
+      stockData.forEach((d) => {
+        coorMap.set(d['data']['name'], { x0: d.x0, x1: d.x1, y0: d.y0, y1: d.y1 })
+      })
+
+      stockTiles
+        .transition()
+        .duration(800)
+        .attr('transform', (d) => {
+          let coordination = coorMap.get(d['data']['name'])
+          return `translate(${coordination.x0},${coordination.y0})`
+        })
+
+      stockTiles
+        .selectAll('rect')
+        .transition()
+        .duration(800)
+        .attr('width', (movie) => {
+          return movie['x1'] - movie['x0']
+        })
+        .attr('height', (movie) => {
+          return movie['y1'] - movie['y0']
+        })
+    }
+  }
+
+  useEffect(() => {
+    if (!dimensions) return
+  }, [isDrawn])
 
   useEffect(() => {
     const svg = select(svgRef.current)
-    svg
-      .selectAll('circle')
-      .data(data)
-      .join('circle')
-      .attr('r', (value) => value)
-      .attr('cx', (value) => value * 2)
-      .attr('cy', (value) => value * 2)
-      .attr('stroke', 'red')
-  }, [data])
+
+    if (!dimensions) return
+    drawTreeMap()
+
+    setIsDrawn(true)
+  }, [data, dimensions])
+
+  let root = treemap(dummyData)
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <div ref={wrapperRef}>
+        <svg ref={svgRef}></svg>
+      </div>
       <button onClick={() => setData(data.map((value) => value + 5))}>Update data</button>
       <button onClick={() => setData(data.filter((value) => value <= 35))}>Filter data</button>
     </React.Fragment>
---
+++ src/dummyData.jsx
@@ -0,0 +1,59 @@
+export let dummyData = {
+  name: 'KODEX200',
+  children: [
+    {
+      name: '삼성전자',
+      value: '70200',
+      ratio: '28.37',
+      color: 'red',
+    },
+    {
+      name: 'SK하이닉스',
+      value: '107000',
+      ratio: '5.07',
+      color: 'blue',
+    },
+    {
+      name: 'NAVER',
+      value: '410500',
+      ratio: '4.43',
+      color: 'green',
+    },
+    {
+      name: '카카오',
+      value: '128500',
+      ratio: '3.15',
+      color: 'yellow',
+    },
+    {
+      name: '삼성SDI',
+      value: '755000',
+      ratio: '3.30',
+      color: 'blacok',
+    },
+    {
+      name: 'LG화학',
+      value: '786000',
+      ratio: '3.11',
+      color: 'orange',
+    },
+    {
+      name: '현대차',
+      value: '215000',
+      ratio: '2.57',
+      color: 'grey',
+    },
+    {
+      name: '셀트리온',
+      value: '209000',
+      ratio: '1.89',
+      color: 'navy',
+    },
+    {
+      name: '기아',
+      value: '88000',
+      ratio: '1.86',
+      color: 'brown',
+    },
+  ],
+}
