a8d3a5c183d40de9a96f6869e9f9a91184dd6b8b
hoyeon <hoyun293@naver.com>
2023-02-01 23:33:02 +0900

    Feature: ETF상세 페이지 컴포넌트 prop 추가

--- src/components/etf-info-view/ETFDetailPage.jsx
+++ src/components/etf-info-view/ETFDetailPage.jsx
@@ -9,14 +9,14 @@ import { Footer } from '../main-view/Footer'
 
 import './ETFDetailPage.scss'
 
-export function ETFDetailPage() {
+export function ETFDetailPage({ info }) {
   return (
     <div className="etf-detail-page-contents">
       <div className="etf-detail-page-item-wrap">
         <ETFSummaryInfoTitle title={'ETF 정보 '} subTitle={'검색된 ETF의 정보입니다.'} />
-        <ETFInfoTable />
+        <ETFInfoTable info={info} />
         <ETFSummaryInfoTitle title={'종목 구성비 '} subTitle={'해당 ETF의 종목 구성비 입니다.'} />
-        <TreeMap />
+        <TreeMap info={info} />
         <ETFSummaryInfoTitle title={'뉴스 '} subTitle={'관련 뉴스입니다.'} />
       </div>
       <Footer />
