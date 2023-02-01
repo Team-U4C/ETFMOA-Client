5411ffd2e074ca5cb836e9229a0638326ea0fd6e
hoyeon <hoyun293@naver.com>
2023-02-01 23:18:13 +0900

    style: ETF 상세정보 API Path Variable로 변경

--- src/utils/api/etf-info-view-api.jsx
+++ src/utils/api/etf-info-view-api.jsx
@@ -1,10 +1,7 @@
 import { config, API } from '../config'
 
 export async function getDetailInfo(name) {
-  const res = await fetch(
-    config.DEV_URL + API.GET_DETAIL + `?name=${encodeURIComponent(name)}`,
-    config.FETCH_HEADER_CONFIG
-  )
+  const res = await fetch(config.DEV_URL + API.GET_DETAIL + `/${name}`, config.FETCH_HEADER_CONFIG)
   const resJSon = await res.json()
   if (resJSon.code === 200) return resJSon.data
 }
