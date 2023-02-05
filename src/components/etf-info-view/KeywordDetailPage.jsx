859aacae2e00053795db98521d37f7f537b58ebf
hoyeon <hoyun293@naver.com>
2023-02-05 18:50:36 +0900

    fix; 연관 ETF 조회 페이지 API 연결
    서버다운으로 인하여 현재는 보여지지 않도록 함

--- src/components/etf-info-view/KeywordDetailPage.jsx
+++ src/components/etf-info-view/KeywordDetailPage.jsx
@@ -1,5 +1,5 @@
 //libraries
-import React from 'react'
+import React, { useState } from 'react'
 
 //components
 import { ETFSummaryInfoTitle } from '../ETFSummaryInfoTitle'
@@ -9,30 +9,33 @@ import { Footer } from '../main-view/Footer'
 
 import './KeywordDetailPage.scss'
 
-export function KeywordDetailPage() {
+export function KeywordDetailPage({ relatedETFs }) {
+  const itemNumPerPage = 20
+
+  const [currentPage, setCurrentPage] = useState(1)
   return (
     <div className="keyword-detail-page-contents">
       <div className="keyword-detail-page-wrap">
         <ETFSummaryInfoTitle title={'연관 ETF '} subTitle={'검색 결과와 연관된 ETF입니다.'} />
-        <div className="keyword-detail-page-search-result">
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <ETFSearchInfo />
-          <div className="splitter"></div>
-          <Pagination totalItemNum={12} itemNumPerPage={3} />
-        </div>
+        {relatedETFs.map((value, index) => {
+          if (index >= (currentPage - 1) * 20 && index <= currentPage * 20 - 1) {
+            return (
+              <div className="keyword-detail-page-search-result" key={index}>
+                <ETFSearchInfo etfInfo={value} />
+                <div className="splitter"></div>
+              </div>
+            )
+          }
+        })}
+
+        <Pagination
+          totalItemNum={relatedETFs.length}
+          itemNumPerPage={itemNumPerPage}
+          onClickEventHandler={(page) => {
+            setCurrentPage(page)
+          }}
+          currentPage={currentPage}
+        />
       </div>
       <Footer />
     </div>
