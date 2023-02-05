c7392d60895e29e97d839224ad027e4cfaf91f55
hoyeon <hoyun293@naver.com>
2023-02-05 19:11:57 +0900

    fix: Header 컴포넌트 더미데이터 처리 및 검색 버튼 작동하도록 수정
    서바다운으로 인하여 검색가능 리스트를 주식1개, ETF1개로 한정합니다.
    
    기존 검색버튼 클릭안되는 현상을 수정합니다.

--- src/components/etf-info-view/Header.jsx
+++ src/components/etf-info-view/Header.jsx
@@ -1,12 +1,12 @@
 //libraries
-import React from 'react'
+import React, {useState} from 'react'
 import ReactSearchBox from 'react-search-box'
 import { useNavigate } from 'react-router-dom'
 
 import './Header.scss'
 export function Header() {
   const navigate = useNavigate()
-
+  const [keyword, setKeyword] = useState('')
   return (
     <div className="etf-info-view-header">
       <div
@@ -21,31 +21,36 @@ export function Header() {
         <div className="bar">
           <ReactSearchBox
             placeholder="종목명 혹은 ETF를 검색하세요"
-            fuseConfigs={{}}
+            autoFocus={true}
             onSelect={(item) => {
               navigate('/detail' + `/${item.item.value}`)
             }}
+   
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                setKeyword('')
+                navigate('/detail' + `/${e.target.defaultValue}`)
+              }
+            }}
+            clearOnSelect={true}
+            onChange={value => setKeyword(value)}
             data={[
               {
                 key: '삼성전자',
                 value: '삼성전자',
               },
               {
-                key: '삼성전자',
-                value: '삼성전자',
+                key: 'TIGER 차이나전기차 SOLACTIVE',
+                value: 'TIGER 차이나전기차 SOLACTIVE ',
               },
-              {
-                key: '삼성전자',
-                value: '삼성전자',
-              },
-              {
-                key: '삼성전자',
-                value: '삼성전자',
-              },
+
             ]}
           />
         </div>
-        <div className="button">
+        <div className="button" onClick={()=>{
+          navigate('/detail' + `/${keyword}`)
+          setKeyword('')
+        }}>
           <img className="button-image" src="/assets/images/mainview/search-svg.svg"></img>
         </div>
       </div>
