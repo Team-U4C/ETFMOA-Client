83b10d5f972647495c831f5e4fcbb1e327b96f07
hoyeon <hoyun293@naver.com>
2023-02-06 23:44:06 +0900

    feat: 연관 ETF 검색 결과 컴포넌트 하드코딩 수정 및 스타일 변경

--- src/components/etf-info-view/ETFSearchInfo.jsx
+++ src/components/etf-info-view/ETFSearchInfo.jsx
@@ -6,6 +6,7 @@ import './ETFSearchInfo.scss'
 import { addComma2Number } from '../../utils/commonFunction'
 
 export function ETFSearchInfo({ etfInfo }) {
+  if(etfInfo === undefined) return <div>Loading...</div>
   return (
     <div className="search-info-row">
       <div className="ci-cell">
@@ -24,26 +25,59 @@ export function ETFSearchInfo({ etfInfo }) {
             className="unit-img"
             src={'/assets/images/components/etf-search-info/triangle-up.png'}
           />
-          <div className="unit">{'95.00'}</div>
+          {etfInfo.diffval !== '0' ? (
+            parseFloat(etfInfo.earn1m) > 0 ? (
+                <div className="unit-up text-sm">{etfInfo.diffval}</div>
+              ) : (
+                <div className="unit-down text-sm">{etfInfo.diffval}</div>
+              )
+            ) : (
+              <div className="unit-same text-sm">{etfInfo.diffval}</div>
+            )}
         </div>
       </div>
       <div className="splitter"></div>
 
       <div className="increment-ratio-one-month-cell">
         <div className="title">{'수익률'}</div>
-        <div className="value">{'-10.23'}</div>
+          {etfInfo.earn1m !== '0.000' ? (
+            parseFloat(etfInfo.earn1m) > 0 ? (
+                <div className="increment-up text-sm">{`${etfInfo.earn1m}%`}</div>
+              ) : (
+                <div className="increment-down text-sm">{`${etfInfo.earn1m}%`}</div>
+              )
+            ) : (
+              <div className="increment-same text-sm">{`${etfInfo.earn1m}%`}</div>
+            )}
+
         <div className="unit">{'1개월'}</div>
       </div>
       <div className="splitter"></div>
 
       <div className="increment-ratio-three-month-cell">
-        <div className="value">{'+40.23'}</div>
+        {etfInfo.earn1m !== '0.000' ? (
+            parseFloat(etfInfo.earn3m) > 0 ? (
+                <div className="increment-up text-sm">{`${etfInfo.earn3m}%`}</div>
+              ) : (
+                <div className="increment-down text-sm">{`${etfInfo.earn3m}%`}</div>
+              )
+            ) : (
+              <div className="increment-same text-sm">{`${etfInfo.earn3m}%`}</div>
+            )}
         <div className="unit">{'3개월'}</div>
       </div>
       <div className="splitter"></div>
 
       <div className="increment-ratio-one-year-cell">
-        <div className="value">{'+123.32'}</div>
+        {etfInfo.earn1m !== '0.000' ? (
+            parseFloat(etfInfo.earn1y) > 0 ? (
+                <div className="increment-up text-sm">{`${etfInfo.earn1y}%`}</div>
+              ) : (
+                <div className="increment-down text-sm">{`${etfInfo.earn1y}%`}</div>
+              )
+            ) : (
+              <div className="increment-same text-sm">{`${etfInfo.earn1y}%`}</div>
+            )}
         <div className="unit">{'1년'}</div>
       </div>
       <div className="splitter"></div>
@@ -56,14 +90,14 @@ export function ETFSearchInfo({ etfInfo }) {
 
       <div className="market-cap-cell">
         <div className="title">{'시가총액'}</div>
-        <div className="value">{addComma2Number(etfInfo.marketcap)}</div>
+        <div className="value">{addComma2Number(etfInfo.marketCap)}</div>
         <div className="unit">{'억'}</div>
       </div>
       <div className="splitter"></div>
 
       <div className="category">
         <div className="title">{'카테고리'}</div>
-        <div className="value">{'원자재'}</div>
+        <div className="value">{etfInfo.category}</div>
       </div>
     </div>
   )
